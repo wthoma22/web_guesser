9c03a960623b5595196c9a1b18999287655ee673
wthoma22 <w.thomas390@yahoo.com>
2017-04-23 19:28:09 -0600

    I4: Colorizing outputs complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,13 +3,15 @@ require 'sinatra/reloader'
 require 'pry'
 
 secret_number = rand(100)
+background = 'white'
 
 get '/' do
   if params["guess"]
     guess = params["guess"].to_i
     message = check_guess(secret_number, guess)
+    background = background_color(message)
   end
-  erb :index, :locals => {:secret_number => secret_number, :message => message}
+  erb :index, :locals => {:secret_number => secret_number, :message => message, :background => background}
 end
 
   def check_guess(secret_number, guess)
@@ -23,5 +25,15 @@ end
       "Too low!"
     else
       "You got it right!"
+    end
+  end
+
+  def background_color(message)
+    case message
+    when "Way too low!" then '#FA5858'
+    when "Way too high!" then '#FA5858'
+    when "Too high!" then '#F5A9A9'
+    when "Too low!" then '#F5A9A9'
+    when "You got it right!" then '#BCF5A9'
     end
   end
